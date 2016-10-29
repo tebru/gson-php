49dc18c15505770fa51740d862a8cfdda442ef0f
Nate Brunette <n@tebru.net>
2016-10-29 12:50:09 -0500

    Adding ClassNameSet model
    Signed-off-by: Nate Brunette <n@tebru.net>

---
+++ src/Internal/Data/ClassNameSet.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Copyright (c) Nate Brunette.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Tebru\Gson\Internal\Data;
+
+use Tebru\Collection\HashSet;
+
+/**
+ * Class ClassNameSet
+ *
+ * A HashSet that is keyed by class name
+ *
+ * @author Nate Brunette <n@tebru.net>
+ */
+final class ClassNameSet extends HashSet
+{
+    /**
+     * Return the key to use for the HashMap
+     *
+     * @param object $element
+     * @return mixed
+     */
+    public function getKey($element)
+    {
+        return get_class($element);
+    }
+}
---
+++ tests/Unit/Internal/Data/ClassNameSetTest.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Copyright (c) Nate Brunette.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Tebru\Gson\Test\Unit\Internal\Data;
+
+use PHPUnit_Framework_TestCase;
+use Tebru\Gson\Internal\Data\ClassNameSet;
+
+/**
+ * Class ClassNameSetTest
+ *
+ * @author Nate Brunette <n@tebru.net>
+ *
+ * @covers \Tebru\Gson\Internal\Data\ClassNameSet
+ */
+class ClassNameSetTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetKeyUsesClass()
+    {
+        $set = new ClassNameSet();
+
+        self::assertSame(ClassNameSetTest::class, $set->getKey($this));
+    }
+}
