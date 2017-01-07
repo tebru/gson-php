1f9be3aab2ea2b0554255cc1b49fd81957e044a7
Nate Brunette <n@tebru.net>
2017-01-06 19:25:52 -0600

    Adding AccessorStrategyFactory
    Signed-off-by: Nate Brunette <n@tebru.net>

---
+++ src/Internal/AccessorStrategyFactory.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Copyright (c) Nate Brunette.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Tebru\Gson\Internal;
+
+use ReflectionMethod;
+use ReflectionProperty;
+use Tebru\Gson\Internal\AccessorStrategy\GetByClosure;
+use Tebru\Gson\Internal\AccessorStrategy\GetByMethod;
+use Tebru\Gson\Internal\AccessorStrategy\GetByPublicProperty;
+use Tebru\Gson\Internal\AccessorStrategy\SetByClosure;
+use Tebru\Gson\Internal\AccessorStrategy\SetByMethod;
+use Tebru\Gson\Internal\AccessorStrategy\SetByPublicProperty;
+
+/**
+ * Class AccessorStrategyFactory
+ *
+ * Creates the strategy that will be used to get or set values
+ *
+ * @author Nate Brunette <n@tebru.net>
+ */
+class AccessorStrategyFactory
+{
+    /**
+     * Create strategy to access a value
+     *
+     * - If there's a method, use the method
+     * - If the property is public, use the property
+     * - Bind a closure
+     *
+     * @param ReflectionProperty $reflectionProperty
+     * @param ReflectionMethod|null $getterMethod
+     * @return GetterStrategy
+     */
+    public function getterStrategy(ReflectionProperty $reflectionProperty, ReflectionMethod $getterMethod = null): GetterStrategy
+    {
+        if (null !== $getterMethod) {
+            return new GetByMethod($getterMethod->getName());
+        } elseif ($reflectionProperty->isPublic()) {
+            return new GetByPublicProperty($reflectionProperty->getName());
+        }
+
+        return new GetByClosure($reflectionProperty->getName(), $reflectionProperty->getDeclaringClass()->getName());
+    }
+
+    /**
+     * Create strategy to set a value
+     *
+     * - If there's a method, use the method
+     * - If the property is public, use the property
+     * - Bind a closure
+     *
+     * @param ReflectionProperty $reflectionProperty
+     * @param ReflectionMethod|null $setterMethod
+     * @return SetterStrategy
+     */
+    public function setterStrategy(ReflectionProperty $reflectionProperty, ReflectionMethod $setterMethod = null): SetterStrategy
+    {
+        if (null !== $setterMethod) {
+            return new SetByMethod($setterMethod->getName());
+        } elseif ($reflectionProperty->isPublic()) {
+            return new SetByPublicProperty($reflectionProperty->getName());
+        }
+
+        return new SetByClosure($reflectionProperty->getName(), $reflectionProperty->getDeclaringClass()->getName());
+    }
+}
---
+++ tests/Unit/Internal/AccessorStrategyFactoryTest.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ * Copyright (c) Nate Brunette.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Tebru\Gson\Test\Unit\Internal;
+
+use PHPUnit_Framework_TestCase;
+use ReflectionMethod;
+use ReflectionProperty;
+use Tebru\Gson\Internal\AccessorStrategy\GetByClosure;
+use Tebru\Gson\Internal\AccessorStrategy\GetByMethod;
+use Tebru\Gson\Internal\AccessorStrategy\GetByPublicProperty;
+use Tebru\Gson\Internal\AccessorStrategy\SetByClosure;
+use Tebru\Gson\Internal\AccessorStrategy\SetByMethod;
+use Tebru\Gson\Internal\AccessorStrategy\SetByPublicProperty;
+use Tebru\Gson\Internal\AccessorStrategyFactory;
+use Tebru\Gson\Test\Mock\ChildClass;
+
+/**
+ * Class AccessorStrategyFactoryTest
+ *
+ * @covers \Tebru\Gson\Internal\AccessorStrategyFactory
+ * @author Nate Brunette <n@tebru.net>
+ */
+class AccessorStrategyFactoryTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetterWithMethod()
+    {
+        $property = new ReflectionProperty(ChildClass::class, 'foo');
+        $method = new ReflectionMethod(ChildClass::class, 'isFoo');
+
+        $factory = new AccessorStrategyFactory();
+        $strategy = $factory->getterStrategy($property, $method);
+
+        self::assertInstanceOf(GetByMethod::class, $strategy);
+    }
+
+    public function testGetterWithProperty()
+    {
+        $property = new ReflectionProperty(ChildClass::class, 'overridden');
+
+        $factory = new AccessorStrategyFactory();
+        $strategy = $factory->getterStrategy($property);
+
+        self::assertInstanceOf(GetByPublicProperty::class, $strategy);
+    }
+
+    public function testGetterWithClosure()
+    {
+        $property = new ReflectionProperty(ChildClass::class, 'foo');
+
+        $factory = new AccessorStrategyFactory();
+        $strategy = $factory->getterStrategy($property);
+
+        self::assertInstanceOf(GetByClosure::class, $strategy);
+    }
+
+    public function testSetterWithMethod()
+    {
+        $property = new ReflectionProperty(ChildClass::class, 'foo');
+        $method = new ReflectionMethod(ChildClass::class, 'setFoo');
+
+        $factory = new AccessorStrategyFactory();
+        $strategy = $factory->setterStrategy($property, $method);
+
+        self::assertInstanceOf(SetByMethod::class, $strategy);
+    }
+
+    public function testSetterWithProperty()
+    {
+        $property = new ReflectionProperty(ChildClass::class, 'overridden');
+
+        $factory = new AccessorStrategyFactory();
+        $strategy = $factory->setterStrategy($property);
+
+        self::assertInstanceOf(SetByPublicProperty::class, $strategy);
+    }
+
+    public function testSetterWithClosure()
+    {
+        $property = new ReflectionProperty(ChildClass::class, 'foo');
+
+        $factory = new AccessorStrategyFactory();
+        $strategy = $factory->setterStrategy($property);
+
+        self::assertInstanceOf(SetByClosure::class, $strategy);
+    }
+}
