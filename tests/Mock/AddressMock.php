bc4097b3193aa44f8c5234cf68cb6f3e586a99aa
Nate Brunette <n@tebru.net>
2017-02-05 15:11:58 -0600

    Adding missing mocks
    Signed-off-by: Nate Brunette <n@tebru.net>

---
+++ tests/Mock/AddressMock.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ * Copyright (c) Nate Brunette.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Tebru\Gson\Test\Mock;
+
+/**
+ * Class AddressMock
+ *
+ * @author Nate Brunette <n@tebru.net>
+ */
+class AddressMock
+{
+    /**
+     * @var string
+     */
+    private $street;
+
+    /**
+     * @var string
+     */
+    private $city;
+
+    /**
+     * @var string
+     */
+    private $state;
+
+    /**
+     * @var int
+     */
+    private $zip;
+
+    /**
+     * @return string
+     */
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param string $street
+     */
+    public function setStreet(string $street)
+    {
+        $this->street = $street;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity(string $city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param string $state
+     */
+    public function setState(string $state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return int
+     */
+    public function getZip(): int
+    {
+        return $this->zip;
+    }
+
+    /**
+     * @param int $zip
+     */
+    public function setZip(int $zip)
+    {
+        $this->zip = $zip;
+    }
+}
---
+++ tests/Mock/Foo.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Copyright (c) Nate Brunette.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Tebru\Gson\Test\Mock;
+
+/**
+ * Class Foo
+ *
+ * @author Nate Brunette <n@tebru.net>
+ */
+class Foo
+{
+
+}
